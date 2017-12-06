d564f9ee2f2443ea9bb2e4375e0bbb6ce61a7a0b
Ruben Cancino <rubencancino6@gmail.com>
2017-12-06 12:53:52 -0600

    Nuevo CancelacionDeCfdi

--- sx-core-plugin/grails-app/domain/sx/core/CancelacionCfdi.groovy
+++ sx-core-plugin/grails-app/domain/sx/core/CancelacionCfdi.groovy
@@ -1,10 +1,29 @@
 package sx.core
 
+import sx.cfdi.Cfdi
+
 class CancelacionCfdi {
 
     String id
+    String motivo
+    //Cfdi cfdi
+    URL acuse
+    //byte[] message
+
+    Date dateCreated
+    Date lastUpdated
+
+    String createUser
+    String updateUser
+
+    static belongsTo = [cfdi: Cfdi]
 
     static constraints = {
+        motivo nullable:true
+        acuse url:true
+        //message maxSize:(1024 * 512)  // 50kb para almacenar el xml
+        createUser nullable:true, maxSize: 100
+        updateUser nullable:true, maxSize: 100
     }
 
     static  mapping={
