7fae55d5785131660cc7f975a2bfde4e65045b73
Ruben Cancino <rubencancino6@gmail.com>
2018-01-09 10:05:32 -0600

    audit log

--- sx-admin-server/grails-app/conf/spring/resources.groovy
+++ sx-admin-server/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,9 @@
+import com.luxsoft.CustomAuditLogListener
+
 // Place your Spring DSL code here
 beans = {
+
+    customAuditLogListener(CustomAuditLogListener) {
+        dataSource = ref('dataSource')
+    }
 }
