32115fe7f15bec41840187b0557511b9934eb8f2
rcancino <rubencancino6@gmail.com>
2018-04-20 18:16:16 -0500

    fix

--- sx-pos-server/grails-app/controllers/sx/compras/CompraController.groovy
+++ sx-pos-server/grails-app/controllers/sx/compras/CompraController.groovy
@@ -2,7 +2,7 @@ package sx.compras
 
 import grails.rest.RestfulController
 import grails.plugin.springsecurity.annotation.Secured
-
+import sx.core.AppConfig
 import sx.core.Folio
 import sx.core.Sucursal
 import sx.reports.ReportService
@@ -20,36 +20,42 @@ class CompraController extends RestfulController{
 
     @Override
     protected List listAllResources(Map params) {
-        params.sort = 'lastUpdated'
+        params.sort = 'fecha'
         params.order = 'desc'
-        params.max = 50
+        params.max = 20
+        def query = Compra.where {}
 
-        def query = Compra.where {sucursal.id == params.sucursal || sucursal.nombre == 'OFICINAS'}
-
-        if(params.boolean('pendientes')){
-            query = query.where {pendiente == true}
+        if(params.boolean('pendientes')) {
+            query = query.where {cerrada == null}
+        }
+        if(params.boolean('transito')){
+            query = query.where {cerrada != null && pendiente == true}
         }
         if( params.folio) {
             query = query.where {folio == params.int('folio') }
         }
         if(params.term) {
-            if(params.term.isInteger()) {
-                query = query.where{folio == params.term.toInteger()}
-            } else {
-                def search = '%' + params.term + '%'
-                query = query.where { proveedor.nombre =~ search || comentario =~ search}
-            }
-
+            def search = '%' + params.term + '%'
+            query = query.where { proveedor.nombre =~ search || comentario =~ search}
         }
-        def list = query.list(params)
-        return list
+        return query.list(params)
+    }
+
+    @Override
+    protected Object createResource() {
+        Compra compra = new Compra()
+        bindData compra, getObjectToBind()
+        compra.folio = 0
+        compra.sucursal = AppConfig.first().sucursal
+        compra.partidas.each { it.sucursal = compra.sucursal}
+        return compra
     }
 
     @Override
     protected Object saveResource(Object resource) {
         resource.folio = Folio.nextFolio('COMPRA','OFICINAS')
-        resource.createdBy = getPrincipal().username
-        return super.saveResource(resource)
+        // resource.createdBy = getPrincipal().username
+        resource.save flush: true
     }
 
     def print( ) {
@@ -59,3 +65,4 @@ class CompraController extends RestfulController{
     }
 
 }
+
