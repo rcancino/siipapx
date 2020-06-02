53f14daab1013b44f47adf4ed9a680783d204cf2
Luis <lquintanillab@gmail.com>
2020-06-02 09:56:25 -0500

    Update LxVentaService.groovy

--- sx-pos-server/grails-app/services/sx/cloud/LxVentaService.groovy
+++ sx-pos-server/grails-app/services/sx/cloud/LxVentaService.groovy
@@ -218,7 +218,7 @@ class LxVentaService implements ApplicationListener<ContextRefreshedEvent>, Even
         
         def direccion = new Direccion()
         
-        direccion.calle = envio.calle
+        direccion.calle = envio.direccion.calle
         direccion.numeroInterior = envio.direccion.numeroInterior
         direccion.numeroExterior = envio.direccion.numeroExterior
         direccion.colonia = envio.direccion.colonia
