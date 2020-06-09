184ab444de9f86bae5648cba9938daa04465ee19
Ruben Cancino <rubencancino6@gmail.com>
2020-06-09 12:50:58 -0500

    Fix CfdiCadenaBuilder33

--- sx-pos-server/src/main/groovy/com/luxsoft/cfdix/v33/CfdiCadenaBuilder33.groovy
+++ sx-pos-server/src/main/groovy/com/luxsoft/cfdix/v33/CfdiCadenaBuilder33.groovy
@@ -1,15 +1,18 @@
 package com.luxsoft.cfdix.v33
 
 import groovy.util.logging.Slf4j
-import lx.cfdi.v33.CadenaBuilder33
 
+import javax.xml.transform.Source
 import javax.xml.transform.Transformer
 import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.stream.StreamResult
 import javax.xml.transform.stream.StreamSource
+import grails.util.Environment
 
 import lx.cfdi.v33.Comprobante
 import lx.cfdi.v33.CfdiUtils
+
 
 @Slf4j
 class CfdiCadenaBuilder33 {
