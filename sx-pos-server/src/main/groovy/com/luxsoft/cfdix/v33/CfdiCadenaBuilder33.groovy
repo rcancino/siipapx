8101ea079193196e407ce7b2007b13f34692b9da
Ruben Cancino <rubencancino6@gmail.com>
2020-06-09 11:56:05 -0500

    Update CadenaBuilder33 to use local files

--- sx-pos-server/src/main/groovy/com/luxsoft/cfdix/v33/CfdiCadenaBuilder33.groovy
+++ sx-pos-server/src/main/groovy/com/luxsoft/cfdix/v33/CfdiCadenaBuilder33.groovy
@@ -17,15 +17,20 @@ class CfdiCadenaBuilder33 {
     // String xsltUrl = "http://www.sat.gob.mx/sitio_internet/cfd/3/cadenaoriginal_3_3/cadenaoriginal_3_3.xslt"
     // String xsltUrl = "http://www.papelsa.com.mx/cfdi/cadenaoriginal_3_3.xslt"
 
+    /*
     CadenaBuilder33 builder = new CadenaBuilder33()
 
     String build(Comprobante comprobante) {
         log.debug('Generando cadena original para comprobante {}', comprobante.folio)
         return builder.build(comprobante)
     }
+    */
 
-    /*
+    String xsltUrl = "http://www.sat.gob.mx/sitio_internet/cfd/3/cadenaoriginal_3_3/cadenaoriginal_3_3.xslt"
+    // tring xsltUrl = "http://www.papelsa.com.mx/cfdi/cadenaoriginal_3_3.xslt"
+
     private Transformer transformer;
+
     String build(Comprobante comprobante){
         log.debug('Generando cadena original para comprobante {}', comprobante.folio)
         // Build transformer
@@ -40,18 +45,34 @@ class CfdiCadenaBuilder33 {
 
         transformer.transform(xmlSource, target)
         String cadena = writer.toString()
-        log.debug('Cadena generada: {}', cadena)
+        log.debug('Cadena original generada: {}', cadena)
         return cadena
     }
-
-    Transformer getTransformer() {
+    /*
+    public Transformer getTransformer() {
         if (!this.transformer) {
-            log.debug('Generando javax.xml.transform.Transformer para {}', this.xsltUrl)
             TransformerFactory factory=TransformerFactory.newInstance()
-            StreamSource source	= new StreamSource(xsltUrl)
+            StreamSource source = new StreamSource(xsltUrl)
             this.transformer = factory.newTransformer(source)
         }
         return this.transformer;
+    }
+    */
+    Transformer getTransformer() {
+        if (!this.transformer) {
+            TransformerFactory factory=TransformerFactory.newInstance()
+            StreamSource source
+            if(Environment.current == Environment.DEVELOPMENT) {
+                source = new StreamSource(new File('/Users/rubencancino/dumps/xslt/cadenaoriginal_3_3.xslt'))
+                // source = new StreamSource('http://www.sat.gob.mx/sitio_internet/cfd/3/cadenaoriginal_3_3/cadenaoriginal_3_3.xslt')
+            } else {
+                source = new StreamSource(new File('/home/xslt/cadenaoriginal_3_3.xslt'))
+            }
+            //StreamSource source   = new StreamSource(xsltUrl)
+            // StreamSource source = new StreamSource(new File('/Users/rubencancino/dumps/cfdi/cadenaoriginal_3_3.xslt'))
+            this.transformer = factory.newTransformer(source)
+        }
+        return this.transformer
     }
 
     StreamSource buildSource(Comprobante comprobante) {
@@ -59,8 +80,5 @@ class CfdiCadenaBuilder33 {
         Reader reader = new StringReader(xml)
         return  new StreamSource(reader)
     }
-
-    */
-
 
 }
